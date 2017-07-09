06d90f1841a9f184be37ee3baad087fdbfe3aef5
Dexter St-Pierre <Dexter.stpierre@gmail.comgit config --global.namegit config --global user.namegit config --global user.email Dexter.stpierre@gmail.com>
2017-07-09 10:27:44 -0500

    recieves task list from server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,18 +1,37 @@
 console.log("js sourced");
 $(document).ready(function(){
   console.log("jquery sourced");
+  refreshTasks();
+  addClickListeners();
+})//end document readt
+
+function addNewTask(task){
+  var newTask = $('#newTask').val();
+  console.log(newTask);
+  $('#newTask').val("");
+  $.ajax({
+    type: "POST",
+    url: '/addTask',
+    data: {newTask: newTask},
+    success: function(response){
+      console.log('new task added');
+    }
+  })
+}
+
+function addClickListeners(){
   $("#submit").on('click', function(){
     console.log('submit button clicked');
-    var newTask = $('#newTask').val();
-    console.log(newTask);
-    $('#newTask').val("");
-    $.ajax({
-      type: "POST",
-      url: '/addTask',
-      data: {newTask: newTask},
-      success: function(response){
-        console.log('new task added');
-      }
-    })
+    addNewTask();
   })
-})
+}
+
+function refreshTasks(){
+  $.ajax({
+    type: "GET",
+    url: '/tasks',
+    success: function(response){
+      console.log(response);
+    }
+  })
+}
--- server/server.js
+++ server/server.js
@@ -14,30 +14,54 @@ var config = {
 };
 var pool = new pg.Pool(config);
 
+app.get('/tasks', function(req, res){
+  console.log('request recieved');
+  pool.connect(function(errorConnectingToDatabase, db, done){
+    if(errorConnectingToDatabase) {
+      console.log('Error connecting to the database.');
+      res.sendStatus(500);
+    } else {
+      var queryText = 'SELECT "task_name" FROM "tasks";';
+      db.query(queryText, function(errorMakingQuery, result){
+        done();
+        if(errorMakingQuery) {
+          console.log('Attempted to query with', queryText);
+          console.log('Error making query');
+          res.sendStatus(500);
+        } else {
+          //console.log(queryText);
+          res.send({tasks: result.rows});
+        }
+      }); // end query
+    } // end if
+  }); // end pool
+  //res.send('recieved');
+})
+
 app.post('/addTask', function(req, res){
   console.log('Task recieved');
   var newTask = req.body.newTask;
   console.log(newTask);
   pool.connect(function(errorConnectingToDatabase, db, done){
-      if(errorConnectingToDatabase) {
-        console.log('Error connecting to the database.');
-        res.sendStatus(500);
-      } else {
-        var queryText = 'INSERT INTO "tasks" ' +
-            '("task_name", "complete") VALUES ($1, \'false\');';
-        db.query(queryText,[newTask], function(errorMakingQuery, result){
-          done();
-          if(errorMakingQuery) {
-            console.log('Attempted to query with', queryText);
-            console.log('Error making query');
-            res.sendStatus(500);
-          } else {
-            console.log(queryText);
-            res.send({tasks: result.rows});
-          }
-        }); // end query
-      } // end if
-    }); // end pool
+    if(errorConnectingToDatabase) {
+      console.log('Error connecting to the database.');
+      res.sendStatus(500);
+    } else {
+      var queryText = 'INSERT INTO "tasks" ' +
+      '("task_name", "complete") VALUES ($1, \'false\');';
+      db.query(queryText,[newTask], function(errorMakingQuery, result){
+        done();
+        if(errorMakingQuery) {
+          console.log('Attempted to query with', queryText);
+          console.log('Error making query');
+          res.sendStatus(500);
+        } else {
+          console.log(queryText);
+          res.sendStatus(200);
+        }
+      }); // end query
+    } // end if
+  }); // end pool
 })
 
 app.get('/*', function(req, res){
